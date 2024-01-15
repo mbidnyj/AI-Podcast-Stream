e20a95281e568d672fe8cfee84d70c40bdf28cb6
mbidnyj <mbidnyj@gmail.com>
2024-01-15 19:52:12 +0100

    public server links updated

--- AI Podcast Stream/Utilities/Constants.swift
+++ AI Podcast Stream/Utilities/Constants.swift
@@ -9,11 +9,11 @@ import Foundation
 
 struct Constants {
     // URLs, Keys, and any other constants
-    static let getAuth = "http://localhost:8080/api/getAuth"
-    static let getCover = "http://localhost:8080/api/getCover"
-    static let getAudioStream = "http://localhost:8080/api/getAudioStream"
+//    static let getAuth = "http://localhost:8080/api/getAuth"
+//    static let getCover = "http://localhost:8080/api/getCover"
+//    static let getAudioStream = "http://localhost:8080/api/getAudioStream"
     
-//    static let getAuth = "https://orca-app-ilspx.ondigitalocean.app/api/getAuth"
-//    static let getCover = "https://orca-app-ilspx.ondigitalocean.app/api/getCover"
-//    static let getAudioStream = "https://orca-app-ilspx.ondigitalocean.app/api/getAudioStream"
+    static let getAuth = "https://orca-app-ilspx.ondigitalocean.app/api/getAuth"
+    static let getCover = "https://orca-app-ilspx.ondigitalocean.app/api/getCover"
+    static let getAudioStream = "https://orca-app-ilspx.ondigitalocean.app/api/getAudioStream"
 }
